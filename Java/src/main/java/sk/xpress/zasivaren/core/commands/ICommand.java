98a6acd752859b696244d5a4124e24e41fae1c84
12XpresS12 <12XpresS12@gmail.com>
2020-11-07 15:39:46 +0100

    System for commands, simple register with Annotation @Command

--- Java/src/main/java/sk/xpress/zasivaren/core/commands/ICommand.java
+++ Java/src/main/java/sk/xpress/zasivaren/core/commands/ICommand.java
@@ -1,9 +1,10 @@
 package sk.xpress.zasivaren.core.commands;
 
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public interface ICommand {
 
-    public void onCommand(User user, String command, String[] args, String arg);
+    public void onCommand(User user, String command, String[] args, String arg, MessageReceivedEvent event);
 
 }
--- Java/src/main/java/sk/xpress/zasivaren/run/commands/Test.java
+++ Java/src/main/java/sk/xpress/zasivaren/run/commands/Test.java
@@ -1,18 +1,21 @@
 package sk.xpress.zasivaren.run.commands;
 
 import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import sk.xpress.zasivaren.core.commands.Command;
 import sk.xpress.zasivaren.core.commands.ICommand;
 
 public class Test implements ICommand {
 
     @Command(command = "ping", description = {"", "cte"}, aliases = {"a", "b"})
-    public void onCommand(User user, String command, String[] args, String arg) {
+    public void onCommand(User user, String command, String[] args, String arg, MessageReceivedEvent event) {
             System.out.println("[COMMAND]: " + user.getName() + ", CMD: " + command + ", ARGS: " +  args + ", ARG:" + arg);
             if(args != null)
             for(String s : args){
                 System.out.println("ARGS: " + s);
             }
+
+            event.getChannel().sendMessage("SUPER, JANA").queue();
     }
 
 }
